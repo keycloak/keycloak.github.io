824e08eb4ede727d3dae1e57a0e846c739cb501e
stianst <stianst@gmail.com>
2018-01-05 17:03:15 +0100

    Web sources moved to https://github.com/keycloak/keycloak-web

--- watch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=`dirname $0`/src/web
-
-while true; do 
-    inotifywait -r -e modify,attrib,close_write,move,create,delete $DIR/config.json $DIR/news $DIR/pages $DIR/resources $DIR/templates $DIR/versions && ./build.sh
-done
